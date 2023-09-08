10f4dfccb9d670b37c38f1bdee5ff3e58fb4ee2b
Zach <zkdoran@gmail.com>
2023-09-08 18:09:44 -0400

    webpack optimizations

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
+// environment.js
 const { environment } = require('@rails/webpacker')
+
+const path = require('path')
+
+const customConfig = {
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, '..', '..', 'app/javascript/src'),
+    }
+  }
+}
+
+environment.config.merge(customConfig);
+
+environment.splitChunks()
 
 module.exports = environment
