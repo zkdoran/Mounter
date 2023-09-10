e3dee412794b167d802481fd7ae2d7e8c57bc63b
Zach <zkdoran@gmail.com>
2023-09-10 17:28:56 -0400

    setup mounts db

--- db/migrate/20230910205830_create_mounts.rb
+++ db/migrate/20230910205830_create_mounts.rb
@@ -1,6 +1,13 @@
 class CreateMounts < ActiveRecord::Migration[6.1]
   def change
     create_table :mounts do |t|
+      t.integer :mount_id
+      t.string :name
+      t.integer :display_id
+      t.string :description
+      t.string :source
+      t.string :faction
+      t.string :requirements
 
       t.timestamps
     end
