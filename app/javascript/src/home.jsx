89e9ac0ec9e5d78fcdd06394f6b08daac3fc80fa
Zach <zkdoran@gmail.com>
2023-09-10 00:08:52 -0400

    testing various things, nothing substantial yet

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -1,40 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { handleErrors } from '@utils/fetchHelper';
+import 'dotenv/config';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 import './home.scss';
 
 class Home extends React.Component {
   state = {
     characterMounts: [],
+    detailedMounts: [],
   }
 
   componentDidMount() {
-    fetch('https://us.api.blizzard.com/profile/wow/character/illidan/ralegna/collections/mounts?namespace=profile-us&locale=en_US&access_token=USMXsBRyA5TSqH0xQ6YMPYYDjuYZCUetC2')
-      .then(handleErrors)
-      .then(data => {
-        console.log(data);
-        this.setState({
-          characterMounts: data.mounts,
-        })
-      })
+
   }
 
   render() {
     const { characterMounts } = this.state;
-
+    
     return (
-      <div className="home">
-        <h1>Home</h1>
-        <ul>
-          {characterMounts.map(mount => (
-            <li key={mount.mount.id}>
-              <img src={mount.mount.icon} alt="image" />
-              <p>{mount.mount.name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <React.Fragment>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Mounter</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col text-center banner py-5">
+              <h1>Mounter</h1>
+              <p>Mounter is a web application that allows you to track your mounts in World of Warcraft.</p>
+            </div>
+          </div>
+          <div className="row">
+
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
