7ef437b21b2b59e3f8c1885cbaa2aec816661234
Zach <zkdoran@gmail.com>
2023-09-13 16:55:10 -0400

    removing characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 module Api
   class User < ApplicationRecord
     has_many :sessions
-    has_many :characters
 
     validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 64 }
     validates :password, presence: true, length: { minimum: 8, maximum: 64 }
