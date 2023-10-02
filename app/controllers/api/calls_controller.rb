e92fbc800e1d6bf3334590cc566ea1f05fd22d44
Zach <zkdoran@gmail.com>
2023-10-02 17:30:43 -0400

    formatted data for ease of use, reduce unused

--- app/controllers/api/calls_controller.rb
+++ app/controllers/api/calls_controller.rb
@@ -11,21 +11,17 @@ module Api
     end
 
     def realms     
-      us_realm = BlizzardApi::Wow::Realm.new region: 'us'
+      us_data = BlizzardApi::Wow::Realm.new.index region: 'us'
+      format_realm(us_data)
 
-      us_data = us_realm.index
+      eu_data = BlizzardApi::Wow::Realm.new.index region: 'eu'
+      format_realm(eu_data)
 
-      eu_realm = BlizzardApi::Wow::Realm.new region: 'eu'
+      kr_data = BlizzardApi::Wow::Realm.new.index region: 'kr'
+      format_realm(kr_data)
 
-      eu_data = eu_realm.index
-
-      kr_realm = BlizzardApi::Wow::Realm.new region: 'kr'
-
-      kr_data = kr_realm.index
-
-      tw_realm = BlizzardApi::Wow::Realm.new region: 'tw'
-
-      tw_data = tw_realm.index
+      tw_data = BlizzardApi::Wow::Realm.new.index region: 'tw'
+      format_realm(tw_data)
 
       realm_data = {
         us: us_data[:realms],
@@ -38,29 +34,31 @@ module Api
     end
 
     def races
-      race = BlizzardApi::Wow::PlayableRace.new
+      race_data = BlizzardApi::Wow::PlayableRace.new.index
 
-      race_data = race.index
+      format_races(race_data)
 
-      render json: race_data, status: :ok
+      render json: race_data[:races], status: :ok
     end
 
     def classes
       class_data = BlizzardApi::Wow::PlayableClass.new.index
 
-      render json: class_data, status: :ok
+      format_classes(class_data)
+
+      render json: class_data[:classes], status: :ok
     end
 
     def mounts
-      mount = BlizzardApi::Wow::Mount.new
-
-      mount_index = mount.index
+      mount_index = BlizzardApi::Wow::Mount.new.index
 
       mount_index[:mounts].each do |mount|
         mount[:mount_detail] = BlizzardApi::Wow.mount.get(mount[:id])
       end
 
-      render json: mount_index, status: :ok
+      format_mounts(mount_index)
+
+      render json: mount_index[:mounts], status: :ok
     end
 
     def profile
@@ -70,7 +68,66 @@ module Api
 
       character_data[:mounts] = character.mounts(params[:realm], params[:character])
 
-      render json: character_data, status: :ok
+      formatted_data = format_characters(character_data)
+
+      render json: formatted_data, status: :ok
     end
+
+    private
+
+    def format_realm(data)
+      data[:realms].each do |realm|
+        realm[:name] = realm[:name][:en_US]
+        realm[:slug] = realm[:slug]
+        realm.delete(:key)
+      end
+    end
+
+    def format_classes(data)
+      data[:classes].each do |classes|
+        classes[:name] = classes[:name][:en_US]
+        classes.delete(:key)
+      end
+    end
+
+    def format_races(data)
+      data[:races].each do |race|
+        race[:name] = race[:name][:en_US]
+        race.delete(:key)
+      end
+    end
+
+    def format_mounts(data)
+      data[:mounts].each do |mount|
+        mount[:name] = mount[:name][:en_US]
+        mount.delete(:key)
+        mount[:mount_detail][:creature_displays] = mount[:mount_detail][:creature_displays][0][:id]
+        mount[:mount_detail].delete(:_links)
+        mount[:mount_detail].delete(:name)
+        mount[:mount_detail].delete(:id)
+        mount[:mount_detail].delete(:description)
+      end
+    end
+
+    def format_characters(data)     
+      data[:mounts][:mounts].each do |mount|
+        mount[:name] = mount[:mount][:name][:en_US]
+        mount[:id] = mount[:mount][:id]
+        mount.delete(:mount)
+        mount.delete(:is_useable)
+      end
+
+      formatted_character = {
+        name: data[:name],
+        realm: data[:realm][:slug],
+        faction: data[:faction][:name][:en_US],
+        race: data[:race][:name][:en_US],
+        character_class: data[:character_class][:name][:en_US],
+        mounts: data[:mounts][:mounts]
+      }
+
+      return formatted_character
+    end
+
   end
 end
