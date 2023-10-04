6eb781f9418ef1037a6bbb25764ccf60d5c5b64b
Zach <zkdoran@gmail.com>
2023-10-03 21:09:38 -0400

    filter "working" need to set reset

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -21,10 +21,14 @@ class Home extends React.Component {
     userRegion: '',
     userRealm: '',
     userCharacter: '',
-    source: ['ACHIEVEMENT', 'DISCOVERY', 'DROP', 'PETSTORE', 'PROFESSION', 'PROMOTION', 'QUEST', 'TCG', 'VENDOR', 'WORLDEVENT'],
+    source: ['Achievement', 'Discovery', 'Drop', 'In-Game Shop', 'Profession', 'Promotion', 'Quest', 'Trading Card Game', 'Vendor', 'World Event'],
     searchError: '',
     listChoice: '',
     mountDisplay: [],
+    sourceFilter: '',
+    raceFilter: '',
+    classFilter: '',
+    factionFilter: '',
   }
 
   componentDidMount() {
@@ -100,7 +104,7 @@ class Home extends React.Component {
           buttonDisabled: false,
         })
       })
-      .then(() => this.mountFilter())
+      .then(() => this.mountListSplit())
       .catch(error => {
         console.log(error)
         this.setState({
@@ -109,7 +113,7 @@ class Home extends React.Component {
       })
   }
 
-  mountFilter = () => {
+  mountListSplit = () => {
     const { characterData, mounts } = this.state;
     const characterMounts = characterData.mounts.map(mount => mount.id);
   
@@ -150,6 +154,30 @@ class Home extends React.Component {
   handleCharacterChange = (e) => {
     this.setState({
       userCharacter: e.target.value,
+    })
+  }
+
+  handleSourceChange = (e) => {
+    this.setState({
+      sourceFilter: e.target.value,
+    })
+  }
+
+  handleRaceChange = (e) => {
+    this.setState({
+      raceFilter: e.target.value,
+    })
+  }
+
+  handleClassChange = (e) => {
+    this.setState({
+      classFilter: e.target.value,
+    })
+  }
+
+  handleFactionChange = (e) => {
+    this.setState({
+      factionFilter: e.target.value,
     })
   }
 
@@ -221,8 +249,69 @@ class Home extends React.Component {
     }
   }
 
+  mountFilter = () => {
+    const { mountDisplay, sourceFilter, raceFilter, classFilter, factionFilter } = this.state;
+
+    if (sourceFilter === '' && raceFilter === '' && classFilter === '' && factionFilter === '') {
+      return mountDisplay;
+    }
+  
+    const filteredMounts = mountDisplay.filter(mount => {
+      let sourceCondition = true;
+      let raceCondition = true;
+      let classCondition = true;
+      let factionCondition = true;
+  
+      if (sourceFilter !== '') {
+        sourceCondition = mount.mount_detail.source === sourceFilter;
+      }
+  
+      if (raceFilter !== '') {
+        if ('requirements' in mount.mount_detail && 'races' in mount.mount_detail.requirements) {
+          raceCondition = mount.mount_detail.requirements.races === raceFilter;
+        } else {
+          raceCondition = false;
+        }
+      } else {
+        if ('requirements' in mount.mount_detail && 'races' in mount.mount_detail.requirements) {
+          raceCondition = false;
+        }
+      }
+  
+      if (classFilter !== '') {
+        if ('requirements' in mount.mount_detail && 'classes' in mount.mount_detail.requirements) {
+          classCondition = mount.mount_detail.requirements.classes === classFilter;
+        } else {
+          classCondition = false;
+        }
+      } else {
+        if ('requirements' in mount.mount_detail && 'classes' in mount.mount_detail.requirements) {
+          classCondition = false;
+        }
+      }
+
+      if (factionFilter !== '') {
+        if ('requirements' in mount.mount_detail && 'faction' in mount.mount_detail.requirements) {
+          factionCondition = mount.mount_detail.requirements.faction === factionFilter;
+        } else {
+          factionCondition = false;
+        }
+      } else {
+        if ('requirements' in mount.mount_detail && 'faction' in mount.mount_detail.requirements) {
+          factionCondition = false;
+        }
+      }
+  
+      return sourceCondition && raceCondition && classCondition && factionCondition;
+    });
+  
+    console.log(filteredMounts);
+  };
+  
+  
+
   render() {
-    const { region, races, classes, mountDisplay, buttonDisabled, realmList, searchError } = this.state;
+    const { region, races, classes, mountDisplay, buttonDisabled, realmList, searchError, source } = this.state;
     
     return (
       <div className="home">
@@ -282,7 +371,7 @@ class Home extends React.Component {
               <div className="alert alert-danger" role="alert">
                 {searchError}
               </div>
-              : null
+              : <br />
             }
           </div>
           <div className="row filters">
@@ -291,6 +380,44 @@ class Home extends React.Component {
               <button className="btn btn-primary" disabled={buttonDisabled} onClick={this.listChoice} value="collected">Collected</button>
               <button className="btn btn-primary" disabled={buttonDisabled} onClick={this.listChoice} value="uncollected">Not Collected</button>
             </div>
+            <div className="col">
+              <select className="col faction" onChange={this.handleFactionChange}>
+                <option>Select a Faction</option>
+                <option value="Alliance">Alliance</option>
+                <option value="Horde">Horde</option>
+              </select>
+            </div>
+            <div className="col">
+              <select className="col source" onChange={this.handleSourceChange}>
+                <option>Select a Source</option>
+                {source.map(source => {
+                  return (
+                    <option key={source} value={source}>{source}</option>
+                  )
+                })}
+              </select>
+            </div>
+            <div className="col">
+              <select className="col races" onChange={this.handleRaceChange}>
+                <option>Select a Race</option>
+                {races.map(race => {
+                  return (
+                    <option key={race.id} value={race.name}>{race.name}</option>
+                  )
+                })}
+              </select>
+            </div>
+            <div className="col">
+              <select className="col classes" onChange={this.handleClassChange}>
+                <option>Select a Class</option>
+                {classes.map(classs => {
+                  return (
+                    <option key={classs.id} value={classs.name}>{classs.name}</option>
+                  )
+                })}
+              </select>
+            </div>
+            <button className="col" onClick={this.mountFilter}>Filter</button>
           </div>
           <div className="row mounts">
             <h1 className="source">Mounts</h1>
@@ -308,7 +435,7 @@ class Home extends React.Component {
                         }} />
                         <div className="card-body">
                           <h5 className="card-title">{mount.name}</h5>
-                          {mount.mount_detail.source ? <p className="card-text">Source: {mount.mount_detail.source.name.en_US}</p> : <p className="card-text">Source: Unknown</p>}
+                          {mount.mount_detail.source ? <p className="card-text">Source: {mount.mount_detail.source}</p> : <p className="card-text">Source: Unknown</p>}
                           <a href={`https://www.wowhead.com/mount/${mount.id}`} target="_blank">Wowhead</a>
                         </div>
                       </div>
