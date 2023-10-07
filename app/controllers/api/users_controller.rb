3198e04dbcebf945a5b7456ed36ff0edb207e271
Zach <zkdoran@gmail.com>
2023-10-07 18:13:13 -0400

    added email requirement for sign up

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,7 +12,7 @@ module Api
       private
 
       def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password, :email)
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :characters, through: :rosters
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 64 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 3, maximum: 64 }
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
 
   after_validation :hash_password
