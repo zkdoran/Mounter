42b07c1f4432da0eb4c36ba3fc35948d5fa14cdb
Zach <zkdoran@gmail.com>
2023-10-07 17:07:41 -0400

    added checker for character existance

---
+++ app/services/character_existence_checker.rb
@@ -0,0 +1,17 @@
+class CharacterExistenceChecker
+  BlizzardApi.configure do |config|
+    config.app_id = ENV['BLIZZARD_CLIENT_ID']
+    config.app_secret = ENV['BLIZZARD_CLIENT_SECRET']
+    config.region = 'us'
+    config.use_cache = true
+    config.redis_host = ENV['REDIS_HOST']
+    config.redis_port = ENV['REDIS_PORT']
+    config.cache_access_token = true
+  end
+
+  def self.character_exists?(region, realm, character_name)
+    character = BlizzardApi::Wow::CharacterProfile.new region: region
+    character_data = character.get(realm, character_name)
+    character_data.key?(:name)
+  end
+end
