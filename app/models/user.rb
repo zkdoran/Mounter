70c0a46e216ec85c14fc7ed845e13d18e2813871
Zach <zkdoran@gmail.com>
2023-10-07 17:06:21 -0400

    link user to characters through roster

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :sessions
+  has_many :rosters
+  has_many :characters, through: :rosters
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 64 }
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
