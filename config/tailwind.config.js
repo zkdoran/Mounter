a06d5aff0aa39c97438d131fd68225bc29f1a5aa
Zach <zkdoran@gmail.com>
2023-10-11 18:16:45 -0400

    daisy ui theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,10 +14,28 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a0f7c3",
+          "secondary": "#c3ed6f",
+          "accent": "#edb682",
+          "neutral": "#1e1b23",
+          "base-100": "#474747",
+          "info": "#9aa8df",
+          "success": "#1bda64",
+          "warning": "#f0a133",
+          "error": "#e32b40",
+        },
+      },
+    ],
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
+    require('daisyui'),
   ]
 }
