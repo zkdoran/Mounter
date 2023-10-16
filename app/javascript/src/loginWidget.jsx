959372e583d60a98b185aed24384171a0ec7895f
Zach <zkdoran@gmail.com>
2023-10-16 17:43:33 -0400

    made widgets for signup, login, and logout

---
+++ app/javascript/src/loginWidget.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { handleErrors, safeCredentials } from '@utils/fetchHelper';
+
+class LoginWidget extends React.Component {
+  state = {
+    username: '',
+    password: '',
+    error: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  // Login call
+  login = (e) => {
+    const { username, password } = this.state;
+    e.preventDefault();
+
+    fetch('/api/sessions', safeCredentials({
+      method: 'POST',
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+        }
+      })
+    }))
+      .then(handleErrors)
+      .then(response => {
+        if (response.success) {
+          this.setState({
+            username: '',
+            password: '',
+            email: '',
+          })
+        }
+      })
+      .then(() => {
+        this.props.onLogin();
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          error: 'Error logging in',
+        })
+      })
+  }
+  
+  render () {
+    const { username, password, error } = this.state;
+    return (
+      <React.Fragment>
+        <form onSubmit={this.login}>
+          <div className="">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              className="input input-bordered input-secondary w-full max-w-xs"
+              value={username}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div className="">
+            <label>Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input input-bordered input-secondary w-full max-w-xs"
+              value={password}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-active btn-primary">Login</button>
+          { error && 
+            <div className="alert alert-error">
+              <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+              <span>{error}</span>
+            </div>
+          }
+        </form>
+        <hr />
+        <p className="">Don't have an account? <a className="" onClick={this.props.toggle}>Sign up</a></p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default LoginWidget;
---
+++ app/javascript/src/logoutWidget.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { safeCredentials, handleErrors } from '@utils/fetchHelper';
+
+class LogoutWidget extends React.Component {
+  state = {
+    logoutError: '',
+  }
+
+  // Logout call
+  endSession = () => {
+    fetch('/api/sessions/logout', safeCredentials({
+      method: 'DELETE',
+    }))
+      .then(handleErrors)
+      .then(response => {
+        if (response.success) {
+          this.setState({
+            loggedIn: false,
+          })
+        }
+      })
+      .then(() => {
+        this.props.onLogout();
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          logoutError: 'Error logging out',
+        })
+      })
+  }
+
+  render () {
+    const { logoutError } = this.state;
+    return (
+      <React.Fragment>
+        <p className="py-4">Are you sure you want to log out?</p>
+        <button className="btn btn-active btn-error" onClick={this.endSession}>Log Out</button>
+        {logoutError && 
+          <div className="alert alert-error py-4">
+            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <span>{logoutError}</span>
+          </div>
+        }
+      </React.Fragment>
+    )
+  }
+}
+
+export default LogoutWidget;
